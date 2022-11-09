f8c92df3a06d3c0b77753b0d9221aad4e1e7e350
devsilva <jerson.silva99@hotmail.com>
2022-11-09 12:51:00 -0500

    Añadiendo rutas iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import { Home, NotFound } from './utils/import'
 
 const App = () => {
   return <>
-  
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   </>
 }
 
