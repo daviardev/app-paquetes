f1d6d9bf46344ecc02c6934290633c77a8e89da0
devsilva <jerson.silva99@hotmail.com>
2022-11-14 18:30:31 -0500

    refactor: Import login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import { Home, NavBar, NotFound } from './utils/import'
+import { Home, NavBar, NotFound, Login } from './utils/import'
 
 const App = () => {
   return <>
@@ -9,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='*' element={<NotFound />} />
+      <Route path='/ingresar' element={<Login />} />
     </Routes>
   </>
 }
