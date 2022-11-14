697b91750df845e2f9103e03acf4019250d1e532
devsilva <jerson.silva99@hotmail.com>
2022-11-14 12:56:56 -0500

    feat: Create navbar component

---
+++ src/containers/NavBar/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { NavLink, Link } from 'react-router-dom'
+
+import './styles.scss'
+
+const NavBar = () => {
+    const [toggle, setToggle] = useState(false);
+  
+    return <>
+      <header className='app-navbar'>
+        <section className="navbar-container">
+          <NavLink to='/' className="app-navbar-logo">
+            <img src="/src/assets/favicon.svg" alt="logo del proyecto" />
+            <span>pqentrega</span>
+          </NavLink>
+        <a href="#open" className='app-navbar-menu-btn' onClick={() => setToggle(!toggle)}>
+          <span id='open-menu' className={`navbar-burguer ${toggle && 'navbar-burguer-anim'}`} />
+        </a>
+      <nav className={`nav-menu ${toggle && 'nav-toggle'}`}>
+        <NavLink onClick={() => setToggle(false)} to='/'>Inicio</NavLink>
+        <NavLink onClick={() => setToggle(false)} to='/envio'>Envio</NavLink>
+        <NavLink onClick={() => setToggle(false)} to='/sobre nosotros'>Sobre nosotros</NavLink>
+        <NavLink onClick={() => setToggle(false)} to='/contacto'>Contacto</NavLink>
+      </nav>
+      </section>
+        <Link to='/ingresar'>
+          <button className="btn btn-button">Ingresar</button>
+        </Link>
+    </header>
+    </>
+}
+
+export default NavBar
