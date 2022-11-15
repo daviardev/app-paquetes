5865915b5557100580ce840d83d245f6023c727b
devsilva <jerson.silva99@hotmail.com>
2022-11-15 11:31:37 -0500

    refactor: Imports page

--- src/utils/import.js
+++ src/utils/import.js
@@ -1,8 +1,21 @@
 // Import pages
 import Home from '../pages/Home'
 import NotFound from '../pages/NotFound'
+import Login from '../pages/Login'
+
+// Import containers
+export { default as NavBar } from '../containers/NavBar'
+export { default as Footer } from '../containers/Footer'
+
+// Import icons
+import { FaUserAlt } from 'react-icons/fa'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 export {
     Home,
-    NotFound
+    Login,
+    NotFound,
+    FaUserAlt,
+    AiOutlineMenu,
+    AiOutlineClose
 }
