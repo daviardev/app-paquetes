94e8b61bc0b36caba87e41cc3f2dc6dc9868ea2c
devsilva <jerson.silva99@hotmail.com>
2022-11-15 11:28:25 -0500

    fix: Fixing footer allways down

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import './styles.scss'
+
 const Home = () => {
     return <>
-        <h1>Aquí se verá reflejado todo lo de inicio</h1>
+    <div className="home__container">
+        <h2>pqentrega</h2>
+        <h3>La distancia más corta entre usted y sus paquetes no es una línea récta</h3>
+    </div>
     </>
 }
 
