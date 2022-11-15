90c146d227b79c4e0a2f4b9b1a6bc61eb716b823
devsilva <jerson.silva99@hotmail.com>
2022-11-15 11:28:06 -0500

    feat: Create notFound page complete

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import { NavLink } from 'react-router-dom'
+
+import './styles.scss'
+
 const NotFound = () => {
     return <>
-        <h1>La página no fue encontrada</h1>
+       <div className="frem">
+        <p>404</p>
+        <h2>Parece que estás perdido</h2>
+        <h5>La página a la que intenta acceder, no está disponible</h5>
+        <NavLink to="/">Ir al inicio</NavLink>
+    </div>
     </>
 }
 
