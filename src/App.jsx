074d6686d55dee23a6807f1bbb229b38c9ccd4cc
devsilva <jerson.silva99@hotmail.com>
2022-11-15 09:18:42 -0500

    feat: Import breackpoints for make responsive footer and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import { Home, NavBar, NotFound, Login } from './utils/import'
+import { Home, NavBar, NotFound, Login, Footer } from './utils/import'
 
 const App = () => {
   return <>
@@ -11,6 +11,7 @@ const App = () => {
       <Route path='*' element={<NotFound />} />
       <Route path='/ingresar' element={<Login />} />
     </Routes>
+    <Footer />
   </>
 }
 
