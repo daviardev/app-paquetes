a58636e3f14e40c76bd694693eac63298ab8d860
devsilva <jerson.silva99@hotmail.com>
2022-11-17 19:07:58 -0500

    Estilando y finalizando Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 
 import { WiDirectionRight } from 'react-icons/wi'
 
+import { Card } from '../../utils/import'
+
 import './styles.scss'
 
 const Home = () => {
@@ -10,10 +12,11 @@ const Home = () => {
     <div className="home__container">
         <div className="card__content">
             <div className="card__line" />
+            <h1 className='card__title'>pqentrega</h1>
             <p className='card__p'>La distancia mas corta entre usted y sus paquetes no es una linea recta, es PQENTREGA.</p>
             <div className="search__container">
                 <p>Consultar envío</p>
-                <input type="text" placeholder=' Guia'/>
+                <input type="text" placeholder='Ingresa el # de guia'/>
                 <Link to='/envio'>
                     <button>
                         <WiDirectionRight className='icon' />
@@ -22,6 +25,20 @@ const Home = () => {
             </div>
         </div>
         <div className='img'/>
+        <br /> <br />
+        <h2 className='title'>Conoce nuestros servicios</h2>
+        <Card
+            title='Soluciones digitales'
+            text='Entregas electrónicas, SMS, email masivo y más para publicidad, legales y otros.'
+        />
+        <Card
+            title='Soluciones de Transporte y Entrega'
+            text='Recogemos en tu puerta y entregamos en el tiempo que requieras.'
+        />
+        <Card
+            title='Soluciones de Logística'
+            text='Optimizamos tu logística para campañas y tu almacenamiento.'
+        />
     </div>
     </>
 }
