2c861b1a709271a3311ae4354871286b86cd61e5
devsilva <jerson.silva99@hotmail.com>
2022-11-18 22:04:23 -0500

    Importando

--- src/utils/import.js
+++ src/utils/import.js
@@ -1,7 +1,9 @@
 // Import pages
 import Home from '../pages/Home'
-import NotFound from '../pages/NotFound'
 import Login from '../pages/Login'
+import Invoice from '../pages/Invoice'
+import NotFound from '../pages/NotFound'
+import SendPackage from '../pages/SendPackage'
 
 // Import containers
 export { default as NavBar } from '../containers/NavBar'
@@ -11,11 +13,17 @@ export { default as Footer } from '../containers/Footer'
 import { FaUserAlt } from 'react-icons/fa'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
+// import components
+import Card from '../components/Card'
+
 export {
     Home,
+    Card,
     Login,
+    Invoice,
     NotFound,
     FaUserAlt,
+    SendPackage,
     AiOutlineMenu,
     AiOutlineClose
 }
