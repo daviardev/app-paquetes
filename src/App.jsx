f983b1e3ec61b071c87b4a51dd521776bc3f799a
devsilva <jerson.silva99@hotmail.com>
2022-11-22 11:28:54 -0500

    refactor: Import context

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import { Home, NavBar, NotFound, Login, Footer, SendPackage, Invoice } from './utils/import'
+import { AuthContext } from './context/AuthContext'
+import { Home, NavBar, NotFound, Footer, SendPackage, Invoice, Detail } from './utils/import'
 
 const App = () => {
   return <>
   <NavBar />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='*' element={<NotFound />} />
-      <Route path='/ingresar' element={<Login />} />
-      <Route path='/envio' element={<SendPackage />} />
-      <Route path='/factura' element={<Invoice />} />
-    </Routes>
-    <Footer />
+    <AuthContext>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+        <Route path='/detalle' element={<Detail />} />
+        <Route path='/envio' element={<SendPackage />} />
+        <Route path='/factura' element={<Invoice />} />
+      </Routes>
+    </AuthContext>
+  <Footer />
   </>
 }
 
