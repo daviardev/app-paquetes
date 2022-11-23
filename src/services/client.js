0a1962d3772969456bf043f019bb0948302f01f4
devsilva <jerson.silva99@hotmail.com>
2022-11-23 18:56:02 -0500

    refactor: Remove authentication google, and only use auth with email

--- src/services/client.js
+++ src/services/client.js
@@ -1,6 +1,5 @@
 import { getAuth } from 'firebase/auth'
 import { initializeApp } from 'firebase/app'
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
@@ -13,16 +12,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
-const authGoogle = getAuth(app)
-
-export const loginGoogle = () => {
-    const googleProvider = new GoogleAuthProvider()
-    signInWithPopup(authGoogle, googleProvider).then(res => {
-        const name = res.user.displayName
-        const email = res.user.email
-
-        console.log(`El nombre es: ${name}\nEl correo es: ${email}`)
-    }).catch(err => {
-        console.error(err)
-    })
-}
+export const auth = getAuth(app)
