04074427a226ee7c9e61f3c76160b98d4541cfde
devsilva <jerson.silva99@hotmail.com>
2022-11-23 18:51:40 -0500

    Envolviendo las rutas con context

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import { AuthContext } from './context/AuthContext'
-import { Home, NavBar, NotFound, Footer, SendPackage, Invoice, Detail } from './utils/import'
+import { AuthProvider } from './context/AuthProvider'
+import { Home, NavBar, NotFound, Footer, SendPackage, Invoice, Detail, Login } from './utils/import'
 
 const App = () => {
   return <>
   <NavBar />
-    <AuthContext>
+    <AuthProvider>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='*' element={<NotFound />} />
-        <Route path='/detalle' element={<Detail />} />
-        <Route path='/envio' element={<SendPackage />} />
-        <Route path='/factura' element={<Invoice />} />
+          <Route path='*' element={<NotFound />} />
+          <Route path='/detalle' element={<Detail />} />
+          <Route path='/envio' element={<SendPackage />} />
+          <Route path='/factura' element={<Invoice />} />
+          <Route path='/iniciar-sesion' element={<Login />} />
       </Routes>
-    </AuthContext>
+    </AuthProvider>  
   <Footer />
   </>
 }
