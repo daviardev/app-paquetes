80f9aacb4261e8912e4e51479bd8f5a37a36a1df
devsilva <jerson.silva99@hotmail.com>
2022-11-25 10:44:32 -0500

    feat: Create detail page

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,26 +1,106 @@
 import React from 'react'
 
-import { useAuthContext } from '../../context/AuthContext'
-
-import './styles.scss'
-
 const Detail = () => {
+    return (
+        <div className='container'>
+            <header>DETALLES</header>
+            <form>
+            <div className='form first'>
+                <div className='details personal'>
+                    <strong className='title'>Remitente / Origen</strong>
+                    <h1>Guia #3218334</h1>
+                    <div className='fields'>
+                        <div className='input-field'>
+                            <strong>Ciudad de recogida</strong>
+                            <p>Barranquilla</p>
+                        </div>
 
-    const appContext = useAuthContext()
-    console.log(appContext)
-    const number = 192345236
-    return <>
-        <div className="detail__container">
-            <section className="wrapper">
-                <div className="clear" />
-                <section className="detail__tabs">
-                    <div className="detail__guide">
-                        <h1>Guía # {number} </h1>
+                        <div className='input-field'>
+                            <strong>Ciudad de destino</strong>
+                            <p>Bogotá</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Cédula de contacto</strong>
+                            <p>1045759468</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong htmlFor='typePackage'>Fecha de entrega</strong>
+                            <p>Hoy</p>
+                        </div>
+
+                        <div className="input-field">
+                            <strong>Número de contacto</strong>
+                            <p>3003683126</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Nombre contacto</strong>
+                            <p>Jerson David Silva Arjona</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Dirección</strong>
+                            <p>calle 47 #21B-69</p>
+                        </div>
+                        
+                        <div className='input-field'>
+                            <strong>Tipo de paquete</strong>
+                            <p>Documento</p>
+                        </div>
                     </div>
-                </section>
-            </section>
+                </div>
+
+                <div className='details ID'>
+                    <strong className='title'>Destinatario / Destino</strong>
+
+                    <div className='fields'>
+                    <div className='input-field'>
+                            <strong>Ciudad de recogida</strong>
+                            <p>Barranquilla</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Ciudad de destino</strong>
+                            <p>Bogotá</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Cédula de contacto</strong>
+                            <p>1046349120</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Fecha de entrega</strong>
+                            <p>Hoy</p>
+                        </div>
+
+                        <div className="input-field">
+                            <strong>Número de contacto</strong>
+                            <p>3118797141</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Nombre contacto</strong>
+                            <p>Joel Cantillo</p>
+                        </div>
+
+                        <div className='input-field'>
+                            <strong>Dirección</strong>
+                            <p>Cra sur 32 #21-44</p>
+                        </div>
+                        
+                        <div className='input-field'>
+                            <strong>Tipo de paquete</strong>
+                            <p>Documento</p>
+                        </div>
+                    </div>
+                </div> 
+            </div>
+            </form>
         </div>
-    </>
+    )
 }
 
 export default Detail
