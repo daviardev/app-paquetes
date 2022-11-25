ca872e81681ac93b501d86d8e82655545a7d1ff7
devsilva <jerson.silva99@hotmail.com>
2022-11-25 10:42:49 -0500

    remove: Remove context

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
-import { AuthProvider } from './context/AuthProvider'
-import { Home, NavBar, NotFound, Footer, SendPackage, Invoice, Detail, Login } from './utils/import'
+import { Home, NavBar, NotFound, Footer, SendPackage, Invoice, Detail } from './utils/import'
 
 const App = () => {
   return <>
-  <NavBar />
-    <AuthProvider>
-      <Routes>
-        <Route path='/' element={<Home />} />
-          <Route path='*' element={<NotFound />} />
-          <Route path='/detalle' element={<Detail />} />
-          <Route path='/envio' element={<SendPackage />} />
-          <Route path='/factura' element={<Invoice />} />
-          <Route path='/iniciar-sesion' element={<Login />} />
-      </Routes>
-    </AuthProvider>  
-  <Footer />
+    <NavBar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='*' element={<NotFound />} />
+      <Route path='/detalle' element={<Detail />} />
+      <Route path='/envio' element={<SendPackage />} />
+      <Route path='/factura' element={<Invoice />} />
+    </Routes>
+    <Footer />
   </>
 }
 
