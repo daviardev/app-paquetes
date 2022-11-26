28d1807f0c1fa258b24feab1bb9d9b2f6b5b9da7
devsilva <jerson.silva99@hotmail.com>
2022-11-25 22:47:50 -0500

    feat: Get datas for the detail page

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -1,103 +1,128 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { db } from '../../services/client'
+import { collection, getDocs } from 'firebase/firestore'
 
 const Detail = () => {
+    const [get, setGet] = useState([])
+
+    useEffect(() => {
+        const getPackage = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, 'envios'))
+                const docs = []
+                querySnapshot.forEach((doc) => {
+                    docs.push({ ...doc.data(), id: doc.id })
+                })
+                setGet(docs)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getPackage()
+    }, [get])
     return (
         <div className='container'>
             <header>DETALLES</header>
             <form>
-            <div className='form first'>
-                <div className='details personal'>
-                    <strong className='title'>Remitente / Origen</strong>
-                    <h1>Guia #3218334</h1>
-                    <div className='fields'>
-                        <div className='input-field'>
-                            <strong>Ciudad de recogida</strong>
-                            <p>Barranquilla</p>
-                        </div>
+                {
+                    get.map(data => (
+                        <div className='form first'>
+                            <div className='details personal'>
+                                <strong className='title'>Remitente / Origen</strong>
+                                <h1>Guia #3218334</h1>
+                                <div className='fields'>
+                                    <div className='input-field'>
+                                        <strong>Ciudad de recogida</strong>
+                                        <p>{data.ciudad_cliente}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Ciudad de destino</strong>
-                            <p>Bogotá</p>
-                        </div>
+                                    <div className='input-field'>
+                                        <strong>Ciudad de destino</strong>
+                                        <p>{data.ciudad_recibe}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Cédula de contacto</strong>
-                            <p>1045759468</p>
-                        </div>
+                                    <div className='input-field'>
+                                        <strong>Cédula de contacto</strong>
+                                        <p>{data.cc_cliente}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong htmlFor='typePackage'>Fecha de entrega</strong>
-                            <p>Hoy</p>
-                        </div>
+                                    <div className='input-field'>
+                                        <strong htmlFor='typePackage'>Fecha de entrega</strong>
+                                        <p>Hoy</p>
+                                    </div>
 
-                        <div className="input-field">
-                            <strong>Número de contacto</strong>
-                            <p>3003683126</p>
-                        </div>
+                                    <div className="input-field">
+                                        <strong>Número de contacto</strong>
+                                        <p>{data.telefono_cliente}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Nombre contacto</strong>
-                            <p>Jerson David Silva Arjona</p>
-                        </div>
+                                    <div className='input-field'>
+                                        <strong>Nombre contacto</strong>
+                                        <p>{data.nombre_cliente}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Dirección</strong>
-                            <p>calle 47 #21B-69</p>
-                        </div>
-                        
-                        <div className='input-field'>
-                            <strong>Tipo de paquete</strong>
-                            <p>Documento</p>
-                        </div>
-                    </div>
-                </div>
+                                    <div className='input-field'>
+                                        <strong>Dirección</strong>
+                                        <p>{data.direccion_cliente}</p>
+                                    </div>
 
-                <div className='details ID'>
-                    <strong className='title'>Destinatario / Destino</strong>
+                                    <div className='input-field'>
+                                        <strong>Tipo de paquete</strong>
+                                        <p>{data.paquete}</p>
+                                    </div>
+                                </div>
+                            </div>
 
-                    <div className='fields'>
-                    <div className='input-field'>
-                            <strong>Ciudad de recogida</strong>
-                            <p>Barranquilla</p>
-                        </div>
+                            <div className='details ID'>
+                                <strong className='title'>Destinatario / Destino</strong>
 
-                        <div className='input-field'>
-                            <strong>Ciudad de destino</strong>
-                            <p>Bogotá</p>
-                        </div>
+                                <div className='fields'>
+                                    <div className='input-field'>
+                                        <strong>Ciudad de recogida</strong>
+                                        <p>{data.ciudad_cliente}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Cédula de contacto</strong>
-                            <p>1046349120</p>
-                        </div>
+                                    <div className='input-field'>
+                                        <strong>Ciudad de destino</strong>
+                                        <p>{data.ciudad_recibe}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Fecha de entrega</strong>
-                            <p>Hoy</p>
-                        </div>
+                                    <div className='input-field'>
+                                        <strong>Cédula de contacto</strong>
+                                        <p>{data.cc_recibe}</p>
+                                    </div>
 
-                        <div className="input-field">
-                            <strong>Número de contacto</strong>
-                            <p>3118797141</p>
-                        </div>
+                                    <div className='input-field'>
+                                        <strong>Fecha de entrega</strong>
+                                        <p>Hoy</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Nombre contacto</strong>
-                            <p>Joel Cantillo</p>
-                        </div>
+                                    <div className="input-field">
+                                        <strong>Número de contacto</strong>
+                                        <p>{data.telefono_recibe}</p>
+                                    </div>
 
-                        <div className='input-field'>
-                            <strong>Dirección</strong>
-                            <p>Cra sur 32 #21-44</p>
+                                    <div className='input-field'>
+                                        <strong>Nombre contacto</strong>
+                                        <p>{data.nombre_recibe}</p>
+                                    </div>
+
+                                    <div className='input-field'>
+                                        <strong>Dirección</strong>
+                                        <p>Cra sur 32 #21-44</p>
+                                    </div>
+
+                                    <div className='input-field'>
+                                        <strong>Tipo de paquete</strong>
+                                        <p>{data.paquete}</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        
-                        <div className='input-field'>
-                            <strong>Tipo de paquete</strong>
-                            <p>Documento</p>
-                        </div>
-                    </div>
-                </div> 
-            </div>
+                    ))
+                }
+
             </form>
         </div>
     )
